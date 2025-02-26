9b2b7326be14171391787d32725d0a0866ba7037
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-26 21:48:07 +0000

    Atualizações finais do desafio Super Trunfo tema 2

--- SuperTrunfo2.c
+++ SuperTrunfo2.c
@@ -107,7 +107,7 @@ int main() {
     char nome2[50] = "Retirolândia";
     int populacao2 = 2, pontos_tur2 = 2;
     float area2 = 40;
-    double pib2 = 40;
+    double pib2 = 35.2;
 
     //Cálculo dos outros dados da carta 1
     float densidadepop1, pibpercapita1;
@@ -119,88 +119,210 @@ int main() {
     densidadepop2 = (float) populacao2 / area2;
     pibpercapita2 = (float) pib2 / populacao2;
 
-
     //Menu Principal
 
-    int escolhaMenu;
+    char escolhaMenu1, escolhaMenu2;
 
     printf("\n--- Menu Principal ---\n\n");
-    printf("Com qual propriedade deseja batalhar?\n");
-    printf("1. População\n");
-    printf("2. Área\n");
-    printf("3. PIB\n");
-    printf("4. Pontos Turísticos\n");
-    printf("5. Densidade Populacional\n");
-    printf("6. PIB per capita\n");
-    scanf("%d", &escolhaMenu);
+    printf("Escolha a primeira propriedade para batalha:\n");
+    printf("P. População\n");
+    printf("A. Área\n");
+    printf("B. PIB\n");
+    printf("T. Pontos Turísticos\n");
+    printf("D. Densidade Populacional\n");
+    printf("C. PIB per capita\n");
+    scanf("%c", &escolhaMenu1);
 
-    //Comparação
+    printf("\nEscolha a segunda propriedade para batalha:\n");
+    escolhaMenu1 != 'P' && escolhaMenu1 != 'p' ? printf("P. População\n") : printf("");
+    escolhaMenu1 != 'A' && escolhaMenu1 != 'a' ? printf("A. Área\n") : printf("");
+    escolhaMenu1 != 'B' && escolhaMenu1 != 'b' ? printf("B. PIB\n") : printf("");
+    escolhaMenu1 != 'T' && escolhaMenu1 != 't' ? printf("T. Pontos Turísticos\n") : printf("");
+    escolhaMenu1 != 'D' && escolhaMenu1 != 'd' ? printf("D. Densidade Populacional\n") : printf("");
+    escolhaMenu1 != 'C' && escolhaMenu1 != 'c' ? printf("C. PIB per capita\n") : printf("");
+    scanf(" %c", &escolhaMenu2);
+
+    float soma1 = 0, soma2 = 0;
+
     printf("\n--- Fase de Batalha ---\n\n");
 
-    switch (escolhaMenu){
-        case 1:
+    printf("- Round 1 -\n");
+    switch (escolhaMenu1){
+        case 'P':
+        case 'p':
             if (populacao1 == populacao2){
                 printf("População: %s e %s possuem %d habitantes ambas. Empate!\n", nome1, nome2, populacao1);
             } else if (populacao1 > populacao2){
-                printf("População: %s - %d habitantes x %s - %d habitantes.\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome1);
+                printf("População: %s - %d habitantes x %s - %d habitantes\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome1);
             } else {
-                printf("População: %s - %d habitantes x %s - %d habitantes.\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome2);
+                printf("População: %s - %d habitantes x %s - %d habitantes\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome2);
             }
+            soma1 += populacao1;
+            soma2 += populacao2;
             break;
 
-        case 2:
+        case 'A':
+        case 'a':
             if (area1 == area2){
                 printf("Área: %s e %s possuem %.2f Km² ambas. Empate!\n", nome1, nome2, area1);
             } else if (area1 > area2){
-                printf("Área: %s - %.2f Km² x %s - %.2f Km².\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome1);
+                printf("Área: %s - %.2f Km² x %s - %.2f Km²\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome1);
             } else {
-                printf("Área: %s - %.2f Km² x %s - %.2f Km².\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome2);
+                printf("Área: %s - %.2f Km² x %s - %.2f Km²\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome2);
             }
+            soma1 += area1;
+            soma2 += area2;
             break;
 
-        case 3:
+        case 'B':
+        case 'b':
             if (pib1 == pib2){
                 printf("PIB: %s e %s possuem PIB de R$%.2f ambas. Empate!\n", nome1, nome2, pib1);
             } else if (pib1 > pib2){
-                printf("PIB: %s - R$%.2f x %s - R$%.2f.\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome1);
+                printf("PIB: %s - R$%.2f x %s - R$%.2f\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome1);
             } else {
-                printf("PIB: %s - R$%.2f x %s - R$%.2f.\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome2);
+                printf("PIB: %s - R$%.2f x %s - R$%.2f\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome2);
             }
+            soma1 += pib1;
+            soma2 += pib2;
             break;
 
-        case 4:
+        case 'T':
+        case 't':
             if (pontos_tur1 == pontos_tur2){
                 printf("Pontos Turísticos: %s e %s possuem %d pontos ambas. Empate!\n", nome1, nome2, pontos_tur1);
             } else if (pontos_tur1 > pontos_tur2){
-                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos.\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome1);
+                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome1);
             } else {
-                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos.\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome2);
+                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome2);
             }
+            soma1 += pontos_tur1;
+            soma2 += pontos_tur2;
             break;
         
-        case 5:
+        case 'D':
+        case 'd':
             if (densidadepop1 == densidadepop2){
                 printf("Densidade populacional: %s e %s possuem %.2f ambas. Empate!\n", nome1, nome2, densidadepop1);
             } else if (densidadepop1 < densidadepop2){
-                printf("Densidade Populacional: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome1);
+                printf("Densidade Populacional: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome1);
             } else {
-                printf("Densidade Populacional: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome2);
+                printf("Densidade Populacional: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome2);
             }
+            soma1 += densidadepop1;
+            soma2 += densidadepop2;
             break;
 
-        case 6:
+        case 'C':
+        case 'c':
             if (pibpercapita1 == pibpercapita2){
                 printf("PIB per capita: %s e %s possuem %.2f ambas. Empate!\n", nome1, nome2, pibpercapita1);
             } else if (pibpercapita1 > pibpercapita2){
-                printf("PIB per capita: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome1);
+                printf("PIB per capita: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome1);
             } else {
-                printf("PIB per capita: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome2);
+                printf("PIB per capita: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome2);
             }
+            soma1 += pibpercapita1;
+            soma2 += pibpercapita2;
             break;
             
         default:
-            printf("Opção inválida!");
+            printf("Primeira opção inválida!\n");
             break;
+    }
+
+    printf("\n- Round 2 -\n");
+    switch (escolhaMenu2){
+        case 'P':
+        case 'p':
+            if (populacao1 == populacao2){
+                printf("População: %s e %s possuem %d habitantes ambas. Empate!\n", nome1, nome2, populacao1);
+            } else if (populacao1 > populacao2){
+                printf("População: %s - %d habitantes x %s - %d habitantes\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome1);
+            } else {
+                printf("População: %s - %d habitantes x %s - %d habitantes\nVitória da cidade %s!\n", nome1, populacao1, nome2, populacao2, nome2);
+            }
+            soma1 += populacao1;
+            soma2 += populacao2;
+            break;
+
+        case 'A':
+        case 'a':
+            if (area1 == area2){
+                printf("Área: %s e %s possuem %.2f Km² ambas. Empate!\n", nome1, nome2, area1);
+            } else if (area1 > area2){
+                printf("Área: %s - %.2f Km² x %s - %.2f Km²\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome1);
+            } else {
+                printf("Área: %s - %.2f Km² x %s - %.2f Km²\nVitória da cidade %s!\n", nome1, area1, nome2, area2, nome2);
+            }
+            soma1 += area1;
+            soma2 += area2;
+            break;
+
+        case 'B':
+        case 'b':
+            if (pib1 == pib2){
+                printf("PIB: %s e %s possuem PIB de R$%.2f ambas. Empate!\n", nome1, nome2, pib1);
+            } else if (pib1 > pib2){
+                printf("PIB: %s - R$%.2f x %s - R$%.2f\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome1);
+            } else {
+                printf("PIB: %s - R$%.2f x %s - R$%.2f\nVitória da cidade %s!\n", nome1, pib1, nome2, pib2, nome2);
+            }
+            soma1 += pib1;
+            soma2 += pib2;
+            break;
+
+        case 'T':
+        case 't':
+            if (pontos_tur1 == pontos_tur2){
+                printf("Pontos Turísticos: %s e %s possuem %d pontos ambas. Empate!\n", nome1, nome2, pontos_tur1);
+            } else if (pontos_tur1 > pontos_tur2){
+                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome1);
+            } else {
+                printf("Pontos Turísticos: %s - %d pontos x %s - %d pontos\nVitória da cidade %s!\n", nome1, pontos_tur1, nome2, pontos_tur2, nome2);
+            }
+            soma1 += pontos_tur1;
+            soma2 += pontos_tur2;
+            break;
+        
+        case 'D':
+        case 'd':
+            if (densidadepop1 == densidadepop2){
+                printf("Densidade populacional: %s e %s possuem %.2f ambas. Empate!\n", nome1, nome2, densidadepop1);
+            } else if (densidadepop1 < densidadepop2){
+                printf("Densidade Populacional: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome1);
+            } else {
+                printf("Densidade Populacional: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, densidadepop1, nome2, densidadepop2, nome2);
+            }
+            soma1 += densidadepop1;
+            soma2 += densidadepop2;
+            break;
+
+        case 'C':
+        case 'c':
+            if (pibpercapita1 == pibpercapita2){
+                printf("PIB per capita: %s e %s possuem %.2f ambas. Empate!\n", nome1, nome2, pibpercapita1);
+            } else if (pibpercapita1 > pibpercapita2){
+                printf("PIB per capita: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome1);
+            } else {
+                printf("PIB per capita: %s - %.2f x %s - %.2f\nVitória da cidade %s!\n", nome1, pibpercapita1, nome2, pibpercapita2, nome2);
+            }
+            soma1 += pibpercapita1;
+            soma2 += pibpercapita2;
+            break;
+            
+        default:
+            printf("Segunda opção inválida!\n");
+            break;
+    }
+
+    printf("\n- Round 3 -\n");
+    if(soma1 == soma2){
+        printf("Soma: %s e %s possuem %.2f ambas. Empate!\n", nome1, nome2, soma1);
+    } else if(soma1 > soma2){
+        printf("Soma: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, soma1, nome2, soma2, nome1);
+    } else {
+        printf("Soma: %s - %.2f x %s - %.2f.\nVitória da cidade %s!\n", nome1, soma1, nome2, soma2, nome2);
     }
 
     return 0;
